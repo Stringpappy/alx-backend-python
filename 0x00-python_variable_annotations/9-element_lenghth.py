8fe6e1295383728ca3f16c491dbe90ec39c6a818
stringpappy <jejeodunayo70@gmail.com>
2024-10-10 21:44:54 +0100

    variable annotation

--- 0x00-python_variable_annotations/9-element_lenghth.py
+++ 0x00-python_variable_annotations/9-element_lenghth.py
@@ -4,5 +4,5 @@ from typing import Mapping, MutableMapping, Sequence, Iterable, Tuple, List
 
 
 def element_length(lst: Iterable[Sequence]) -> List[Tuple[Sequence, int]]:
-    """ <F11><F11>Return Element length """
+    """ Return Element length """
     return [(i, len(i)) for i in lst]
