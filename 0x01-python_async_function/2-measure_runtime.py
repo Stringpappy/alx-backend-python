b717fe92ca449718e8c296d9ef453b9fe564fccf
stringpappy <jejeodunayo70@gmail.com>
2024-10-15 21:26:02 +0200

    asynio

--- 0x01-python_async_function/2-measure_runtime.py
+++ 0x01-python_async_function/2-measure_runtime.py
@@ -1,4 +1,4 @@
-#!/usr/bin/env python
+#!/usr/bin/env python3
 """ 
 async
 """
@@ -9,6 +9,7 @@ wait_n = __import__("1-concurrent_cooroutines").wait_n
 
 
 def measure_time(n: int, max_dlay: int) -> float:
+    """func that return the runtime
     start_time = time.time
     asyncio.run(wait_n(n, max_delay))
     return (time.time() - start_time) / n
--- 0x01-python_async_function/4-tasks.py
+++ 0x01-python_async_function/4-tasks.py
@@ -1,3 +1,7 @@
+#!/usr/bin/env python3
+"""
+asyncio
+"""
 import time 
 import asyncio
 from typing import List
@@ -5,6 +9,7 @@ from typing import List
 task_wait_random = __import__("3-tasks.py").wait_n 
 
 async def  task_wait_n( int, max_dekay: int) -> list[float]:
+    """func that take code  from wait_n and alter into new func"""
     wait_times = asyncio.gather(
         *turple(map(lambda _: task_wait_random(max_delay), range(n)
         ))
